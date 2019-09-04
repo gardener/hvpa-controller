2bcca3f3719143f9c25e845e1b88a8c4fa0b7ffe
Gaurav Gupta <gaurav.gupta07@sap.com>
2019-09-04 13:51:47 +0530

    Adding vpa to known types, and other minor fixes

--- controllers/hvpa_controller_test.go
+++ controllers/hvpa_controller_test.go
@@ -17,12 +17,20 @@ limitations under the License.
 package controllers
 
 import (
+	"context"
 	"time"
 
+	autoscalingv1alpha1 "github.com/gardener/hvpa-controller/api/v1alpha1"
 	. "github.com/onsi/ginkgo"
-	// . "github.com/onsi/ginkgo/extensions/table"
-	// . "github.com/onsi/gomega"
+	. "github.com/onsi/ginkgo/extensions/table"
+	. "github.com/onsi/gomega"
+	appsv1 "k8s.io/api/apps/v1"
+	autoscaling "k8s.io/api/autoscaling/v2beta2"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
+	vpa_api "k8s.io/autoscaler/vertical-pod-autoscaler/pkg/apis/autoscaling.k8s.io/v1beta2"
+	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
@@ -37,91 +45,91 @@ const timeout = time.Second * 5
 
 var _ = Describe("#TestReconcile", func() {
 
-	// DescribeTable("##ReconcileHPAandVPA",
-	// 	func(instance *autoscalingv1alpha1.Hvpa) {
+	DescribeTable("##ReconcileHPAandVPA",
+		func(instance *autoscalingv1alpha1.Hvpa) {
 
-	// 		replica := int32(1)
-	// 		deploytest := &appsv1.Deployment{
-	// 			ObjectMeta: metav1.ObjectMeta{
-	// 				Name:      "deploy-test",
-	// 				Namespace: "default",
-	// 			},
-	// 			Spec: appsv1.DeploymentSpec{
-	// 				Replicas: &replica,
-	// 				Selector: &metav1.LabelSelector{
-	// 					MatchLabels: map[string]string{
-	// 						"name": "testDeployment",
-	// 					},
-	// 				},
-	// 				Template: v1.PodTemplateSpec{
-	// 					ObjectMeta: metav1.ObjectMeta{
-	// 						Labels: map[string]string{
-	// 							"name": "testDeployment",
-	// 						},
-	// 					},
-	// 					Spec: v1.PodSpec{
-	// 						Containers: []v1.Container{
-	// 							v1.Container{
-	// 								Name:  "pause",
-	// 								Image: "k8s.gcr.io/pause-amd64:3.0",
-	// 							},
-	// 						},
-	// 					},
-	// 				},
-	// 			},
-	// 		}
-	// 		// Setup the Manager and Controller.  Wrap the Controller Reconcile function so it writes each request to a
-	// 		// channel when it is finished.
-	// 		mgr, err := manager.New(cfg, manager.Options{})
-	// 		Expect(err).NotTo(HaveOccurred())
-	// 		c = mgr.GetClient()
-	// 		//commented this section as we need to relook at how to validate the absence of add function
-	// 		// recFn, requests := SetupTestReconcile(newReconciler(mgr))
-	// 		// Expect(add(mgr, recFn)).NotTo(HaveOccurred())
+			replica := int32(1)
+			deploytest := &appsv1.Deployment{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "deploy-test",
+					Namespace: "default",
+				},
+				Spec: appsv1.DeploymentSpec{
+					Replicas: &replica,
+					Selector: &metav1.LabelSelector{
+						MatchLabels: map[string]string{
+							"name": "testDeployment",
+						},
+					},
+					Template: v1.PodTemplateSpec{
+						ObjectMeta: metav1.ObjectMeta{
+							Labels: map[string]string{
+								"name": "testDeployment",
+							},
+						},
+						Spec: v1.PodSpec{
+							Containers: []v1.Container{
+								v1.Container{
+									Name:  "pause",
+									Image: "k8s.gcr.io/pause-amd64:3.0",
+								},
+							},
+						},
+					},
+				},
+			}
+			// Setup the Manager.
+			mgr, err := ctrl.NewManager(cfg, ctrl.Options{})
+			Expect(err).NotTo(HaveOccurred())
 
-	// 		stopMgr, mgrStopped := StartTestManager(mgr, &GomegaWithT{})
+			reconciler := HvpaReconciler{
+				Client: mgr.GetClient(),
+				Log:    ctrl.Log.WithName("controllers").WithName("Hvpa"),
+				scheme: mgr.GetScheme(),
+			}
 
-	// 		defer func() {
-	// 			close(stopMgr)
-	// 			mgrStopped.Wait()
-	// 		}()
+			err = reconciler.SetupWithManager(mgr)
+			Expect(err).NotTo(HaveOccurred())
 
-	// 		// Create the test deployment
-	// 		err = c.Create(context.TODO(), deploytest)
-	// 		Expect(err).NotTo(HaveOccurred())
+			c = mgr.GetClient()
+			stopMgr, mgrStopped := StartTestManager(mgr, &GomegaWithT{})
 
-	// 		// Create the Hvpa object and expect the Reconcile and HPA to be created
-	// 		err = c.Create(context.TODO(), instance)
-	// 		Expect(err).NotTo(HaveOccurred())
-	// 		defer c.Delete(context.TODO(), instance)
+			defer func() {
+				close(stopMgr)
+				mgrStopped.Wait()
+			}()
 
-	// 		//commented the below line as we need to relook setuptestreconcile
-	// 		//Eventually(requests, timeout).Should(Receive(Equal(expectedRequest)))
+			// Create the test deployment
+			err = c.Create(context.TODO(), deploytest)
+			Expect(err).NotTo(HaveOccurred())
 
-	// 		hpa := &autoscaling.HorizontalPodAutoscaler{}
-	// 		Eventually(func() error { return c.Get(context.TODO(), hpaKey, hpa) }, timeout).
-	// 			Should(Succeed())
+			// Create the Hvpa object and expect the Reconcile and HPA to be created
+			err = c.Create(context.TODO(), instance)
+			Expect(err).NotTo(HaveOccurred())
+			defer c.Delete(context.TODO(), instance)
 
-	// 		vpa := &vpa_api.VerticalPodAutoscaler{}
-	// 		Eventually(func() error { return c.Get(context.TODO(), vpaKey, vpa) }, timeout).
-	// 			Should(Succeed())
+			hpa := &autoscaling.HorizontalPodAutoscaler{}
+			Eventually(func() error { return c.Get(context.TODO(), hpaKey, hpa) }, timeout).
+				Should(Succeed())
 
-	// 		// Delete the HPA and expect Reconcile to be called for HPA deletion
-	// 		Expect(c.Delete(context.TODO(), hpa)).NotTo(HaveOccurred())
-	// 		//commented the below line as we need to relook setuptestreconcile
-	// 		//Eventually(requests, timeout).Should(Receive(Equal(expectedRequest)))
-	// 		Eventually(func() error { return c.Get(context.TODO(), hpaKey, hpa) }, timeout).
-	// 			Should(Succeed())
+			vpa := &vpa_api.VerticalPodAutoscaler{}
+			Eventually(func() error { return c.Get(context.TODO(), vpaKey, vpa) }, timeout).
+				Should(Succeed())
 
-	// 		// Manually delete HPA & VPA since GC isn't enabled in the test control plane
-	// 		Eventually(func() error { return c.Delete(context.TODO(), hpa) }, timeout).
-	// 			Should(MatchError("horizontalpodautoscalers.autoscaling \"foo-hpa\" not found"))
-	// 		Eventually(func() error { return c.Delete(context.TODO(), vpa) }, timeout).
-	// 			Should(MatchError("verticalpodautoscalers.autoscaling.k8s.io \"foo-vpa\" not found"))
+			// Delete the HPA and expect Reconcile to be called for HPA deletion
+			Expect(c.Delete(context.TODO(), hpa)).NotTo(HaveOccurred())
+			Eventually(func() error { return c.Get(context.TODO(), hpaKey, hpa) }, timeout).
+				Should(Succeed())
 
-	// 		// Delete the test deployment
-	// 		Expect(c.Delete(context.TODO(), deploytest)).NotTo(HaveOccurred())
-	// 	},
-	// 	Entry("hvpa", newHvpa()),
-	// )
+			// Manually delete HPA & VPA since GC isn't enabled in the test control plane
+			Eventually(func() error { return c.Delete(context.TODO(), hpa) }, timeout).
+				Should(MatchError("horizontalpodautoscalers.autoscaling \"foo-hpa\" not found"))
+			Eventually(func() error { return c.Delete(context.TODO(), vpa) }, timeout).
+				Should(MatchError("verticalpodautoscalers.autoscaling.k8s.io \"foo-vpa\" not found"))
+
+			// Delete the test deployment
+			Expect(c.Delete(context.TODO(), deploytest)).NotTo(HaveOccurred())
+		},
+		Entry("hvpa", newHvpa()),
+	)
 })
